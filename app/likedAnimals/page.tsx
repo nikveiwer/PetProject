52bc92d5b884d52d9ed378a559d5f8c32e4007bf
nikveiwer <ulanov200219@gmail.come.com>
2023-03-16 13:21:10 +0300

    with beggining of liked animals

--- app/likedAnimals/page.tsx
+++ app/likedAnimals/page.tsx
@@ -1,8 +1,4 @@
-export default function AccountFeaturesLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
+export default function LikedAnimalsPage() {
     return (
         <section className=" min-h-[calc(100vh-172px)] xl:px-40 lg:px-14 py-4 px-3"></section>
     );
